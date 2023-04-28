294c6183972e9a30974a58da44f0c6fd8872bb1b
ngiann <ngiann@gmail.com>
2023-04-28 16:28:41 +0200

    remove PG datasets

--- src/listdatasets.jl
+++ src/listdatasets.jl
@@ -4,21 +4,7 @@
 """
 function listdatasets()
 
-  vcat(listvirialdatasets(), listpgdatasets(), "NGC5548", "Mgc0811", "ESO399-IG20")
-
-end
-
-
-"""
-    listpgdatasets()
-    Returns PG datasets as an array of strings.
-"""
-function listpgdatasets()
-
-    ["pg0026", "pg0052", "pg0804", "pg0844",
-     "pg0953", "pg1211", "pg1226", "pg1229",
-     "pg1307", "pg1351", "pg1411", "pg1426",
-     "pg1613", "pg1617", "pg1700", "pg1704", "pg2130"]
+  vcat(listvirialdatasets(), "Mgc0811", "ESO399-IG20")
 
 end
 
